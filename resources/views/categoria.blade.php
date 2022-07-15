ff7bc2d34aa6994b09d0d7e2ebb50d5e5ce71a7f
Joaquin Nina <Joaquin.nina@tecsup.edu.pe>
2022-07-14 19:53:39 -0500

    proyecto enviado

---
+++ resources/views/categoria.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Categoria de videojuegos</title>
+</head>
+    <h2>Categoria de videojuegos</h2>
+<body>
+    <p>¿Quién no ha jugado alguna vez a un videojuego? El objetivo principal de los videojuegos es el entretenimiento,
+    ya sea para adultos o para los más pequeños de la casa.En el mercado actual hay una gran variedad, por ello os contamos
+    todo sobre los tipos de videojuegos para la PS4 y PS3.
+    </p>
+    <p>De forma generalizada, los videojuegos recrean entornos y situaciones virtuales en los que el videojugador puede
+    controlar a uno o varios personajes (o cualquier otro elemento de dicho entorno), para conseguir uno o varios objetivos
+    por medio de reglas determinadas. No obstante, el mundo de los juegos virtuales es tan extenso que antes de continuar
+    es imprescindible establecer una clasificación de los videojuegos. Primero tendremos en cuenta la diferenciación entre
+    videojuegos para niños y para adultos, posteriormente los clasificaremos por temática, y por último por tipo de
+    videoconsola.
+    </p>
+</body>
+</html>
